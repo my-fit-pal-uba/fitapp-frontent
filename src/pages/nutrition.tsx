ec850183d0052a44ce7d7cd5e46944bf8ee6cb48
Jignacio14 <j14castro2001@gmail.com>
2025-06-09 01:28:26 -0300

    feat: filtros de categoria

--- src/pages/nutrition.tsx
+++ src/pages/nutrition.tsx
@@ -1,32 +1,55 @@
 import { useEffect } from "react";
 import Header from "../components/header";
-import { getDishes } from "../services/nutrition.services";
+import { getDishCategories, getDishes } from "../services/nutrition.services";
 import { Dish } from "../Models/dish";
-
+import "./nutrition.css";
 
 import { useState } from "react";
 import NutritionTable from "../components/nutritiontable";
+import { MealCategory } from "../Models/meal_categorie";
 
 const Nutrition = () => {
     const [dishes, setDishes] = useState<Dish[]>([]);
+    const [categories, setCategories] = useState<MealCategory[]>([]);
 
     useEffect(() => {
         const fetchDishes = async () => {
             const data = await getDishes();
-            console.table(data)
             setDishes(data);
         };
+
+        const fetchDishCategories = async () => {
+            const categories = await getDishCategories();
+            console.table(categories);
+            setCategories(categories);
+        }
+
         fetchDishes();
-    }, []); 
-    
+        fetchDishCategories();
+    }, []);
+
 
     return (
         <>
             <Header />
             <h1 className="nutrition-title">Alimentacion</h1>
+            <div className="category-filter">
+                <h3 className="filter-title">Filtrar por categoría:</h3>
+                <div className="category-buttons">
+                    <button className="category-button active">Todas</button>
+                    {categories.slice(0, 5).map((category) => (
+                        <button
+                            key={category.id}
+                            className="category-button"
+                        >
+                            {category.description}
+                        </button>
+                    ))}
+                </div>
+            </div>
             <div className="nutrition-container">
                 <div className="table-wrapper">
-                    <NutritionTable data={dishes}  />
+                    <NutritionTable data={dishes} />
                 </div>
             </div>
         </>
--- src/services/nutrition.services.ts
+++ src/services/nutrition.services.ts
@@ -10,3 +10,15 @@ export async function getDishes() {
     }
     return data.message;
 }
+
+export async function getDishCategories() {
+    const response = await fetch('http://localhost:8080/nutrition/get_meal_categories');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    if (!data.response) {
+        return []
+    }
+    return data.message;
+}
