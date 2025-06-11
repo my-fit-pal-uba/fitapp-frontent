443bea4e7903fb6cface058b62bc03e4706dbd04
ArielFol <folgueiraariel@gmail.com>
2025-06-11 18:44:43 -0300

    fix style photo

--- src/pages/my_photos.tsx
+++ src/pages/my_photos.tsx
@@ -106,7 +106,7 @@ const PhotosPage = () => {
                 onClick={() => setActivePhoto(null)}
               >
                 <div
-                  className="modal-content relative max-w-full max-h-full p-4"
+                  className="modal-content--photo relative max-w-full max-h-full p-4"
                   onClick={(e) => e.stopPropagation()}
                 >
                   <img
