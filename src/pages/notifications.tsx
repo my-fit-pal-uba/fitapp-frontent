55935c6c387019f08ba9d3d541c10bd193da5040
Jignacio14 <j14castro2001@gmail.com>
2025-06-16 16:22:35 -0300

    feat: add notifications page

---
+++ src/pages/notifications.tsx
@@ -0,0 +1,12 @@
+
+function Notifications() {
+    return (
+        <div>
+            <h1>
+                Notificaciones 
+            </h1>
+        </div>
+    )
+}
+
+export default Notifications;
