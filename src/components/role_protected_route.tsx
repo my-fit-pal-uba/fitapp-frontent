2b93669021b5c17e00a819fc0be8fa3a18ffe29d
ArielFol <folgueiraariel@gmail.com>
2025-06-17 04:22:13 -0300

    feat: restriccion de rutas por rol

---
+++ src/components/role_protected_route.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate } from "react-router";
+import { getToken } from "../Models/token";
+
+interface RoleProtectedRouteProps {
+  allowedRoles: string[];
+  children: JSX.Element;
+}
+
+const RoleProtectedRoute: React.FC<RoleProtectedRouteProps> = ({ allowedRoles, children }) => {
+  const user = getToken();
+
+  if (!user || !allowedRoles.includes(user.rol)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default RoleProtectedRoute;
